d62434f8416b775d42c4861a8b851c41a329cd9d
e6320123 <ben780618@gmail.com>
2021-11-12 09:17:39 +0800

    註冊功能

--- check.php
+++ check.php
@@ -1,39 +1,103 @@
 <?php
 
-
+// 這裡要加header 上機就不用了
 header('Access-Control-Allow-Origin: *');
 
 session_start();
  
 
-$user = $_GET['username'];
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    regist();
+    exit();
+}
+else
+{
+    login();
+    exit();
+}
+ 
 
-$pwd = $_GET['password'];
+function regist(){
 
+    $user = $_POST['username'];
+    
+    $pwd = $_POST['password'];
+    
+    $sql_data = check_member($user, $pwd, 'check');
 
-#檢查是否有此會員
-try {
-	$db = new PDO('mysql:host=localhost;dbname=test','root','');
+    if (count($sql_data) == 1) 
+    {
+        echo "failed... username repeat";
+    }
+    else
+    {
+        #新增會員
+        try { 
+            
+            $db = new PDO('mysql:host=localhost;dbname=test','root','');
+            
+            $sql = "INSERT INTO members (user,pwd) VALUES('{$user}','{$pwd}');";
 
-    $sql = "SELECT * FROM members WHERE user LIKE '{$user}' AND pwd LIKE '{$pwd}';";
-
-	$statement = $db->query($sql); 
-
-    $rows = $statement->rowCount();
-
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    if ($rows ==1) {
-        $_SESSION['login'] = 'yes';
-        $_SESSION['user'] = $results[0]['user'];
+            $statement = $db->query($sql); 
+         
+        }
+        catch (PDOException $e) {
+            echo "<br>";
+            echo $e->getMessage();
+            die();
+        }
+        echo "success";
     }
 
-    echo $rows; 
 }
-catch (PDOException $e) {
-  echo "<br>";
-  echo $e->getMessage();
-  die();
+
+function login(){
+
+    $user = $_GET['username'];
+    
+    $pwd = $_GET['password'];
+    
+    $sql_data = check_member($user,$pwd,'login');
+
+    if (count($sql_data) == 1) 
+    {
+        $_SESSION['login'] = 'yes';
+        $_SESSION['user'] = $sql_data[0]['user'];
+        echo 'success';
+    } 
+   
+}
+
+#檢查是否有此會員 與 檢查帳號密碼
+function check_member($user,$pwd,$check_or_login){
+     try {
+        $sql;
+
+        $db = new PDO('mysql:host=localhost;dbname=test','root','');
+
+        if($check_or_login == 'login')
+        {
+            $sql = "SELECT * FROM members WHERE user LIKE '{$user}' AND pwd LIKE '{$pwd}';";
+        }
+        else
+        {
+            $sql = "SELECT * FROM members WHERE user LIKE '{$user}';";
+        }
+    
+        $statement = $db->query($sql); 
+    
+        $rows = $statement->rowCount();
+    
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+       
+        return $results; 
+    }
+    catch (PDOException $e) {
+      echo "<br>";
+      echo $e->getMessage();
+      die();
+    }
 }
 
 
